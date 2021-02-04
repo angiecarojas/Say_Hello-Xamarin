5213b88118be25c6460f9ba7339f69f5e6a71cbf
angicarojas <angiecarojas@hotmail.com>
2021-02-04 18:23:26 -0500

    first commit

---
+++ Nt_1/Nt_1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Nt_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SayHelloButtonOnClicked(object sender, EventArgs e)
+        {
+            var name = NameEntry.Text;
+            await DisplayAlert("Greeting", $"Hello {name}!", "!Nǐ Hǎo!");
+        }
+    }
+}
